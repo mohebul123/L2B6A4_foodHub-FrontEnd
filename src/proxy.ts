e673e041e31896ed8a8d46deb79de04f687f61aa
mohebul123 <mohebulhassan123@gmail.com>
2026-02-28 23:46:55 +0600

    dashboard is prtoected by proxy function completed

---
+++ src/proxy.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Roles } from "./constants/constant";
+import { getUser } from "./app/service/auth";
+
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const { data } = await getUser();
+
+  if (!data?.user) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  const role = data.user.role;
+
+  if (role === Roles.admin) {
+    if (!pathname.startsWith("/admin-dashboard")) {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    }
+  }
+
+  if (role === Roles.customer) {
+    if (!pathname.startsWith("/customer-dashboard")) {
+      return NextResponse.redirect(new URL("/customer-dashboard", request.url));
+    }
+  }
+
+  if (role === Roles.provider) {
+    if (!pathname.startsWith("/provider-dashboard")) {
+      return NextResponse.redirect(new URL("/provider-dashboard", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/admin-dashboard/:path*",
+    "/customer-dashboard/:path*",
+    "/provider-dashboard/:path*",
+  ],
+};
